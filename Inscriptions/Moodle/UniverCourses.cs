3ec2c8503f0ea091ee54c85adeae54997f6e644e
Univer Dev One <mrangel@univermilenium.edu.mx>
2013-09-03 13:16:28 -0500

    createGroup
    Función para crear grupos

--- Inscriptions/Moodle/UniverCourses.cs
+++ Inscriptions/Moodle/UniverCourses.cs
@@ -6,13 +6,12 @@ using System.Threading.Tasks;
 
 namespace univer.moodle
 {
-    static class UniverCourses
+    public static class UniverCourses
     {
         static public int MPS0101 = 1;
         static public int MDER0101 = 2;
         static public int CRIM0103 = 3;
         static public int MPEG0103 = 4;
         static public int MPEG0418 = 5;
-        static public int MDER0101 = 6;
     }
 }
