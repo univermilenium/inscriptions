85db7e09013a0b90d021c2b04e66ca553926c28b
Univer Dev One <mrangel@univermilenium.edu.mx>
2013-09-19 19:32:30 -0500

    Actualización de query's para alumnos

--- extractions/Querys.cs
+++ extractions/Querys.cs
@@ -33,7 +33,7 @@ namespace univer.extractions
                       CFGPLANES_MST E
   
                     WHERE 
-                      A.ID_ESCUELA = 5 AND A.STATUS = 'A' AND (A.GRADO = 1 OR GRADO = 4) AND 
+                      A.ID_ESCUELA = 5 AND A.STATUS = 'A' AND (A.GRADO = 1 OR A.GRADO = 4) AND 
                       (A.MATRICULA LIKE '580%' OR A.MATRICULA LIKE '584%' OR A.MATRICULA LIKE '581%' OR A.MATRICULA LIKE '582%')	
 
                       AND A.NUMEROALUMNO  = B.NUMEROALUMNO
@@ -78,7 +78,7 @@ namespace univer.extractions
                       CFGPLANES_MST E
   
                     WHERE 
-                      A.ID_ESCUELA = 1 AND A.STATUS = 'A' AND (A.GRADO = 1 OR GRADO = 4) AND 
+                      A.ID_ESCUELA = 1 AND A.STATUS = 'A' AND (A.GRADO = 1 OR A.GRADO = 4) AND 
                       (A.MATRICULA LIKE '180%' OR A.MATRICULA LIKE '184%' OR A.MATRICULA LIKE '181%' OR A.MATRICULA LIKE '182%')		
 
                       AND A.NUMEROALUMNO  = B.NUMEROALUMNO
@@ -109,53 +109,50 @@ namespace univer.extractions
         static public string neza() 
         {
             return @"SELECT DISTINCT
-                      A.MATRICULA,
-                      A.NOMBRE,
-                      A.PATERNO,
-                      A.MATERNO,
-                      A.EMAIL,
-                      B.CODIGOGRUPO,
-                      D.CLAVEASIGNATURA,
-                      A.ID_ESCUELA
-                    FROM
-                      ALUMNOS A,
-                      ALUMNOS_GRUPOS B,
-                      GRUPOS C,
-                      ALUMNOS_KARDEX D,
-                      CFGPLANES_MST E
+                          A.MATRICULA,
+                          A.NOMBRE,
+                          A.PATERNO,
+                          A.MATERNO,
+                          A.EMAIL,
+                          B.CODIGOGRUPO,
+                          D.CLAVEASIGNATURA,
+                          A.ID_ESCUELA
+                        FROM
+                          ALUMNOS A,
+                          ALUMNOS_GRUPOS B,
+                          GRUPOS C,
+                          ALUMNOS_KARDEX D,
+                          CFGPLANES_MST E
   
-                    WHERE 
-                      A.ID_ESCUELA = 2 AND A.STATUS = 'A' AND (A.GRADO = 1 OR A.GRADO = 4) AND 
-                      (A.MATRICULA LIKE '280%' OR A.MATRICULA LIKE '284%' OR A.MATRICULA LIKE '281%' OR A.MATRICULA LIKE '282%')
+                        WHERE 
+                          A.ID_ESCUELA = 2 AND A.STATUS = 'A' AND (A.GRADO = 1 OR A.GRADO = 4) AND 
+                          (A.MATRICULA LIKE '280%' OR A.MATRICULA LIKE '284%' OR A.MATRICULA LIKE '281%' OR A.MATRICULA LIKE '282%')
 
-                      AND A.NUMEROALUMNO  = B.NUMEROALUMNO
-                      AND B.PERIODO       = 1
-                      AND B.CODIGOGRUPO   LIKE 'S%'
-                      AND B.INICIAL       = 2013
-                      AND B.FINAL         = 2013
-                      AND B.CODIGOGRUPO   = C.CODIGOGRUPO
-                      AND C.INICIAL       = 2013
-                      AND C.FINAL         = 2013
-                      AND C.PERIODO       = 1
-                      AND (C.GRADO =1 OR C.GRADO = 4)
-                      --()AND C.GRAD0 =
+                          AND A.NUMEROALUMNO  = B.NUMEROALUMNO
+                          AND B.PERIODO       = 1
+                          AND B.CODIGOGRUPO   LIKE 'S%'
+                          AND B.INICIAL       = 2013
+                          AND B.FINAL         = 2013
+                          AND B.CODIGOGRUPO   = C.CODIGOGRUPO
+                          AND C.INICIAL       = 2013
+                          AND C.FINAL         = 2013
+                          AND C.PERIODO       = 1
+                          AND (C.GRADO =1 OR C.GRADO = 4)  
+                          AND A.NUMEROALUMNO = D.NUMEROALUMNO
+                          AND A.NIVEL = E.NIVEL
+                          AND D.ID_PLAN = E.ID_PLAN
+                          AND D.ID_EVAL = 'A'
+                          AND D.CALIFICACION_1 = 0
   
-                      AND A.NUMEROALUMNO = D.NUMEROALUMNO
-                      AND A.NIVEL = E.NIVEL
-                      AND D.ID_PLAN = E.ID_PLAN
-                      AND D.ID_EVAL = 'A'
-                      AND D.CALIFICACION_1 = 0
-                      --AND A.NIVEL = D.ID_PLAN --*****
+                          AND (
+                            D.CLAVEASIGNATURA = 'MPS0101' 
+                             OR D.CLAVEASIGNATURA = 'CRIM0103' 
+                             OR D.CLAVEASIGNATURA = 'MPEG-0103' 
+                             OR D.CLAVEASIGNATURA = 'MPEG-0418'
+                             OR D.CLAVEASIGNATURA = 'MDER101'
+                            )
   
-                      AND (
-		                    D.CLAVEASIGNATURA = 'MPS0101' 
-                         OR D.CLAVEASIGNATURA = 'CRIM0103' 
-                         OR D.CLAVEASIGNATURA = 'MPEG-0103' 
-                         OR D.CLAVEASIGNATURA = 'MPEG-0418'
-                         OR D.CLAVEASIGNATURA = 'MDER101'
-                        )
-  
-                    order by b.CODIGOGRUPO";
+                        order by b.CODIGOGRUPO";
         }
 
         static public string ixtapaluca() 
@@ -177,7 +174,7 @@ namespace univer.extractions
                       CFGPLANES_MST E
   
                     WHERE 
-                      A.ID_ESCUELA = 3 AND A.STATUS = 'A' AND (A.GRADO = 1 OR GRADO = 4) AND 
+                      A.ID_ESCUELA = 3 AND A.STATUS = 'A' AND (A.GRADO = 1 OR A.GRADO = 4) AND 
                       (A.MATRICULA LIKE '380%' OR A.MATRICULA LIKE '384%' OR A.MATRICULA LIKE '381%' OR A.MATRICULA LIKE '382%')
 
                       AND A.NUMEROALUMNO  = B.NUMEROALUMNO
@@ -212,7 +209,7 @@ namespace univer.extractions
         static public string hidalgo() 
         {
             return @"
-                        SELECT DISTINCT
+                       SELECT DISTINCT
                           A.MATRICULA,
                           A.NOMBRE,
                           A.PATERNO,
@@ -229,7 +226,7 @@ namespace univer.extractions
                           CFGPLANES_MST E
   
                         WHERE 
-                          A.ID_ESCUELA = 4 AND A.STATUS = 'A' AND (A.GRADO = 1 OR GRADO = 4) AND 
+                          A.ID_ESCUELA = 4 AND A.STATUS = 'A' AND (A.GRADO = 1 OR A.GRADO = 4) AND 
                           (A.MATRICULA LIKE '480%' OR A.MATRICULA LIKE '484%' OR A.MATRICULA LIKE '481%' OR A.MATRICULA LIKE '482%')	
 
                           AND A.NUMEROALUMNO  = B.NUMEROALUMNO
