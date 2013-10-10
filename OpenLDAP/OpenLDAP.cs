07cd325fa82ccc21a9d88e26be775622b13d3f54
Univer Dev One <mrangel@univermilenium.edu.mx>
2013-10-10 17:07:25 -0500

    AddUser() para openLDAP

--- OpenLDAP/OpenLDAP.cs
+++ OpenLDAP/OpenLDAP.cs
@@ -31,31 +31,21 @@ namespace univer.LDAP
             return new DirectoryEntry(string.Format("LDAP://{0}/{1}", this.server, this.domain), string.Format("cn={0},{1}", this.admin_username, this.domain), this.admin_password, AuthenticationTypes.None);
         }
 
-        public DirectoryEntry AddUser() 
+        public DirectoryEntry AddUser(UserLDAP user) 
         {
-            DirectoryEntry objUser = this.Entry().Children.Add("cn=user12", "organizationalPerson");
+            DirectoryEntry objUser = this.Entry().Children.Add("cn=" + user.cn, "organizationalPerson");
+
+            objUser.Properties["cn"].Add(user.cn);
+            objUser.Properties["sn"].Add(user.sn);
+            objUser.Properties["seeAlso"].Add(user.seeAlso);
+            objUser.Properties["userPassword"].Add(user.userPassword);
+            objUser.Properties["title"].Add(user.title);
+            objUser.Properties["description"].Add(user.description);
+            objUser.Properties["postalAddress"].Add(user.postalAddress);
+
+            objUser.CommitChanges();
+
+            return objUser;
         }
-
-        public void OpenasdfLDAP() 
-        {
-
-            
-                             string userDn2 = @"cn=admin,dc=online,dc=univer";
-                            string fullPath2 = @"LDAP://192.168.61.39/dc=online,dc=univer";
-                            DirectoryEntry entry = new DirectoryEntry(fullPath2, userDn2, "ldap", AuthenticationTypes.None);
-                            DirectoryEntry objUser = entry.Children.Add("cn=user12", "organizationalPerson");
-
-                            Console.WriteLine(objUser.Path);
-                            objUser.Properties["cn"].Add("user12"); // add this statement can avoid naming violation.
-                            objUser.Properties["sn"].Add("tu12");               //singlename sn
-                            objUser.Properties["title"].Add("student"); //student teacher
-                            string pass = GetMD5("testear");
-                            objUser.Properties["userPassword"].Add("{MD5}"+pass);
-
-                            objUser.CommitChanges();
-             
-
-        }
-
     }
 }
